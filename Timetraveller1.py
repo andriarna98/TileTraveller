3f194ceac51a1e436aef9f9c10f5684e8c5d0ee5
Andri Árnason <andri@Andris-MacBook-Pro.local>
2018-09-18 13:04:15 +0000

    first commit

---
+++ Timetraveller1.py
@@ -0,0 +1,17 @@
+# 9 if statements sem kanna hvar leikmadur er staddur a x/y
+# True/False fyrir directions sem eru i bodi
+# Ef direction er true tha er haegt ad fara i tha att
+# alltaf kannad fyrst ef x er 3 og y er 1, ef thad er satt tha kemur victory
+
+x = 1
+y = 1
+North = True
+West = False
+South = False
+East = False
+
+if x = 1 and y = 1:
+    print("You can travel:", str(direction) + ".")
+    direction = input("Direction:")
+    if direction == True
+
